9c65b618c34ecc6eb2248e8d9e58a86221d3d4fb
FIRMAN SANDY PRAYITNO <i16077@ftis>
2018-11-02 14:33:56 +0700

    Second Commit

--- Calculator.java
+++ Calculator.java
@@ -5,10 +5,16 @@ public class Calculator{
 	}
 
 	public float plus(float a,float b){
-		return a+b;
+		
 	}
 	public float minus(float a,float b){
-		return a-b;
+		
+	}
+	public float multiply(float a,float b){
+
+	}
+	public float division(float a,float b){
+		
 	}
 
 }
