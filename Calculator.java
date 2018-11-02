3d32f7507e7c5654e10e3a5928697f033fd1c55f
Firman Sandy Prayitno <firmansandyp@gmail.com>
2018-11-02 16:47:02 +0700

    Firman: edit method float plus(float,float) dan float minus(float,float)

--- Calculator.java
+++ Calculator.java
@@ -5,10 +5,10 @@ public class Calculator{
 	}
 
 	public float plus(float a,float b){
-		
+		return a+b;
 	}
 	public float minus(float a,float b){
-		
+		return a-b;	
 	}
 	public float multiply(float a,float b){
 
